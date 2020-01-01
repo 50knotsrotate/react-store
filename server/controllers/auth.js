01fe4d91abaaf8650b90f1dfffdf28e11bf94412
= <=>
2020-01-01 12:01:09 -0500

    setup auth endpoints

---
+++ server/controllers/auth.js
@@ -0,0 +1,6 @@
+module.exports = {
+  signup: function(req, res, next) {},
+  signin: function(req, res, next) {},
+  login: function(req, res, next) {},
+  logout: function(req, res, next) {}
+};
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
-const { PORT } = process.env || 4000;
+const PORT = process.env.PORT || 4000;
 
 const morgan = require('morgan');
 // Might not need axios, but I will leave it here just in case I do.
 const axios = require('axios');
+
+const { signin, signup, login,logout } = require('./controllers/auth');
 
 const express = require('express');
 
@@ -11,6 +13,17 @@ const app = express();
 
 app.use(morgan('dev'));
 
+app.post('/signup', signup);
+
+app.post('/signin', signin);
+
+app.post('/login', login);
+
+app.post('/logout', logout);
+
+
 app.listen(PORT, () => { 
     console.log(`Listening on port ${PORT}`)
 })
+
+
