d26fff1e10d40adba005acf4d27f8786fe0165a9
= <=>
2020-01-04 23:01:45 -0500

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { Switch, Route } from 'react-router-dom';
-import store from './ducks/store';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Switch, Route } from "react-router-dom";
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
-    <Provider store={store}>
-      <Switch>
-        {/* <Route />
-        <Route />
-        <Route />
-        <Route />
-        <Route /> 
-        TODO: Routes
-        */}
-      </Switch>
-      </Provider>
+    <Fragment>
+      <NavBar />
+    </Fragment>
   );
 }
 
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+const { Link, Item } = Nav;
+
+export default function NavBar() {
+  return (
+    <Nav class="navbar navbar-expand-lg navbar-dark bg-dark py-3">
+      <a class="navbar-brand" href="#">
+        L.R JENKINS
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarColor01"
+        aria-controls="navbarColor01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul class="navbar-nav mr-auto">
+          <Item class="nav-item active">
+            <Link class="nav-link" href="#">
+              Home 
+            </Link>
+          </Item>
+          <Item class="nav-item">
+            <Link class="nav-link" href="#">
+              Features
+            </Link>
+          </Item>
+          <Item class="nav-item">
+            <Link class="nav-link" href="#">
+              Features
+            </Link>
+          </Item>
+          <Item class="nav-item">
+            <Link class="nav-link" href="#">
+              Features
+            </Link>
+          </Item>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input
+            class="form-control mr-sm-2"
+            type="text"
+            placeholder="Search"
+          />
+          <button class="btn btn-secondary my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </Nav>
+  );
+}
