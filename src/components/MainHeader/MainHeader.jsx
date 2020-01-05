c4955f1dbc9e4b3ee20c9d96499f428d00158f1c
= <=>
2020-01-05 12:42:57 -0500

    signup form POST

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -1,46 +1,34 @@
 import React from "react";
 import "./MainHeader.css";
+import SignUp from "../forms/SignUp/SignUp";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+const axios = require("axios");
 
 export default function MainHeader() {
   return (
-    <div className="jumbotron bg-light">
-      <div class="container-fluid">
-        <div className="row">
-          <div className="col-sm-9">
-                      <h1>Best shop this side of the miss-is-ipp-i!</h1>
-                      
-                      <h2 class = 'text-secondary text-center'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut natus cupiditate debitis officia, sapiente soluta repellat? Eos at non sequi laudantium deleniti consectetur, provident sunt sit repellat quod fugit, optio voluptatem nihil quibusdam! Saepe ut et omnis nesciunt minima praesentium molestiae impedit doloremque, quis, vel quaerat, dolores obcaecati? Eveniet, corrupti?</h2>
-          </div>
-          <div className="col-sm-3">
-            <form id="signup" method="post" action="/signup">
-              <h1>Sign Up!</h1>
-              <input
-                name="user[email]"
-                type="email"
-                placeholder="enter your email"
-                class="input pass"
-              />
-              <input
-                name="user[password]"
-                type="password"
-                placeholder="enter your password"
-                required="required"
-                class="input pass"
-              />
-              <input type="submit" value="Sign me up!" class="inputButton" />
-              <div class="text-center">
-                <a href="#" id="">
-                  create an account
-                </a>{" "}
-                -{" "}
-                <a href="#" id="">
-                  forgot password
-                </a>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Jumbotron>
+      <Container fluid>
+        <Row>
+          <Col sm={7}>
+            <h1>Best shop this side of the miss-is-ipp-i!</h1>
+
+            <h4 class="text-secondary text-center">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut natus
+              cupiditate debitis officia, sapiente soluta repellat? Eos at non
+              sequi laudantium deleniti consectetur, provident sunt sit repellat
+              quod fugit, optio voluptatem nihil quibusdam! Saepe ut et omnis
+              nesciunt minima praesentium molestiae impedit doloremque, quis,
+              vel quaerat, dolores obcaecati? Eveniet, corrupti?
+            </h4>
+          </Col>
+          <Col sm={5}>
+            <SignUp />
+          </Col>
+        </Row>
+      </Container>
+    </Jumbotron>
   );
 }
---
+++ src/components/forms/SignUp/SignUp.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  validateForm = form => {
+    if (1) return true;
+    return false;
+  };
+
+  onSubmit = () => {
+    if (this.validateForm(this.state)) {
+      axios.post("/signup", this.state);
+    }
+  };
+
+  formHandler = (target, value) => {
+    console.log(this.state);
+    this.setState({
+      [target.name]: value
+    });
+  };
+
+  render() {
+    return (
+      <div id="signup">
+        <h1>Sign Up!</h1>
+        <input
+          name="email"
+          type="email"
+          placeholder="enter your email"
+          class="input pass"
+          onChange={e => this.formHandler(e.target, e.target.value)}
+          value={this.state.email}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="enter your password"
+          required="required"
+          class="input pass"
+        />
+        <input
+          type="submit"
+          value="Sign me up!"
+          class="inputButton"
+          onClick={() => {
+            this.onSubmit(this.state);
+          }}
+        />
+        <div class="text-center">
+          <a href="#" id="">
+            create an account
+          </a>{" "}
+          -{" "}
+          <a href="#" id="">
+            forgot password
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
