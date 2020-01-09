170057a07fdc6ed5c424790c0ad61f3276969759
= <=>
2020-01-09 17:30:50 -0500

    remove unneeded styling from shop item image

--- src/components/Cards/FaCard/ShopItems/ShopItems.jsx
+++ src/components/Cards/FaCard/ShopItems/ShopItems.jsx
@@ -8,12 +8,11 @@ export default class ShopItems extends Component {
   }
   render() {
     return (
-      <div className="card border-none" >
+      <div className="card" >
         <img
           className="card-img-top"
           src={this.props.image}
           alt="Card image cap"
-           style={{ width: "250px", height: "250px" }}
         />
         <div className="card-body">
           <h5 className="card-title">{this.props.title}</h5>
