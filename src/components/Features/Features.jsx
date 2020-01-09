63705e79ba27c6aa2493cd4cf2afe84b03721719
= <=>
2020-01-09 16:22:35 -0500

    remove font awesome css

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,27 +1,29 @@
-import React from "react";
+import React, { Fragment} from "react";
 import FaCard from "../Cards/FaCard/FaCard";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 export default function Features() {
-    return (
+  return (
+    <Fragment>
+      <h1>Hi</h1>
       <Row>
-          <Col sm={12} lg={4}>
+        <Col sm={12} lg={4}>
           <FaCard
             type="lock"
             primaryText="Security"
             secondaryText="We always hash your passwords - always"
           />
         </Col>
-          <Col sm={12} lg={4}>
+        <Col sm={12} lg={4}>
           <FaCard
             type="lock"
             primaryText="Security"
             secondaryText="We always hash your passwords - always"
           />
         </Col>
-          <Col sm={12} lg={4}>
+        <Col sm={12} lg={4}>
           <FaCard
             type="lock"
             primaryText="Security"
@@ -29,5 +31,6 @@ export default function Features() {
           />
         </Col>
       </Row>
+    </Fragment>
   );
 }
