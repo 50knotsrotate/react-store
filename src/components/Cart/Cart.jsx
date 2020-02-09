d81f5f366a30cc1904800c5cc6553866492f4ac5
= <=>
2020-02-08 20:44:24 -0500

    finish shopping cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -12,6 +12,49 @@ class Cart extends Component {
 
   componentDidMount() {}
   render() {
+      const { cart } = store.getState();
+      const finalPrice = cart.reduce((acc, curr) => {
+        //   console.log('curr')
+        //   console.log(curr)
+          
+          return acc + (+curr.price * curr.quantity)
+      }, 0)
+
+      console.log('FINAL PRICE')
+      console.log(finalPrice)
+    const items = cart.map((item, i) => (
+      <tr>
+        <th scope="row" class="border-0">
+          <div class="p-2">
+            <img
+              src="https://res.cloudinary.com/mhmd/image/upload/v1556670479/product-1_zrifhn.jpg"
+              alt=""
+              width={70}
+              class="img-fluid rounded shadow-sm"
+            />
+            <div class="ml-3 d-inline-block align-middle">
+              <h5 class="mb-0">
+                {" "}
+                <a href="#" class="text-dark d-inline-block align-middle">
+                  {item.title}
+                </a>
+              </h5>
+            </div>
+          </div>
+        </th>
+        <td class="border-0 align-middle">
+          <strong>{item.price}</strong>
+        </td>
+        <td class="border-0 align-middle">
+          <strong>{item.quantity}</strong>
+        </td>
+        <td class="border-0 align-middle">
+          <a href="#" class="text-dark">
+            <i class="fa fa-trash"></i>
+          </a>
+        </td>
+      </tr>
+    ));
     return (
       <div class="pb-5">
         <div class="container-fluid">
@@ -35,159 +78,13 @@ class Cart extends Component {
                       </th>
                     </tr>
                   </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row" class="border-0">
-                        <div class="p-2">
-                          <img
-                            src="https://res.cloudinary.com/mhmd/image/upload/v1556670479/product-1_zrifhn.jpg"
-                            alt=""
-                            width={70}
-                            class="img-fluid rounded shadow-sm"
-                          />
-                          <div class="ml-3 d-inline-block align-middle">
-                            <h5 class="mb-0">
-                              {" "}
-                              <a
-                                href="#"
-                                class="text-dark d-inline-block align-middle"
-                              >
-                                Timex Unisex Originals
-                              </a>
-                            </h5>
-                            <span class="text-muted font-weight-normal font-italic d-block">
-                              Category: Watches
-                            </span>
-                          </div>
-                        </div>
-                      </th>
-                      <td class="border-0 align-middle">
-                        <strong>$79.00</strong>
-                      </td>
-                      <td class="border-0 align-middle">
-                        <strong>3</strong>
-                      </td>
-                      <td class="border-0 align-middle">
-                        <a href="#" class="text-dark">
-                          <i class="fa fa-trash"></i>
-                        </a>
-                      </td>
-                    </tr>
-                    <tr>
-                      <th scope="row">
-                        <div class="p-2">
-                          <img
-                            src="https://res.cloudinary.com/mhmd/image/upload/v1556670479/product-3_cexmhn.jpg"
-                            alt=""
-                            width={70}
-                            class="img-fluid rounded shadow-sm"
-                          />
-                          <div class="ml-3 d-inline-block align-middle">
-                            <h5 class="mb-0">
-                              <a href="#" class="text-dark d-inline-block">
-                                Lumix camera lense
-                              </a>
-                            </h5>
-                            <span class="text-muted font-weight-normal font-italic">
-                              Category: Electronics
-                            </span>
-                          </div>
-                        </div>
-                      </th>
-                      <td class="align-middle">
-                        <strong>$79.00</strong>
-                      </td>
-                      <td class="align-middle">
-                        <strong>3</strong>
-                      </td>
-                      <td class="align-middle">
-                        <a href="#" class="text-dark">
-                          <i class="fa fa-trash"></i>
-                        </a>
-                      </td>
-                    </tr>
-                    {/* <tr> */}
-                    <tr scope="row">
-                      <div class="p-2">
-                        <img
-                          src="https://res.cloudinary.com/mhmd/image/upload/v1556670479/product-2_qxjis2.jpg"
-                          alt=""
-                          width={70}
-                          class="img-fluid rounded shadow-sm"
-                        />
-                        <div class="ml-3 d-inline-block align-middle">
-                          <h5 class="mb-0">
-                            {" "}
-                            <a href="#" class="text-dark d-inline-block">
-                              Gray Nike running shoe
-                            </a>
-                          </h5>
-                          <span class="text-muted font-weight-normal font-italic">
-                            Category: Fashion
-                          </span>
-                        </div>
-                      </div>
-                      <td class="align-middle">
-                        <strong>$79.00</strong>
-                      </td>
-                      <td class="align-middle">
-                        <strong>3</strong>
-                      </td>
-                      <td class="align-middle">
-                        <a href="#" class="text-dark">
-                          <i class="fa fa-trash"></i>
-                        </a>
-                      </td>
-                    </tr>
-                  </tbody>
+                  <tbody>{items}</tbody>
                 </table>
               </div>
             </div>
           </div>
 
           <div class="row py-5 p-4 bg-white rounded shadow-sm">
-            {/* <div class="col-lg-6">
-              <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">
-                Coupon code
-              </div>
-              <div class="p-4">
-                <p class="font-italic mb-4">
-                  If you have a coupon code, please enter it in the box below
-                </p>
-                <div class="input-group mb-4 border rounded-pill p-2">
-                  <input
-                    type="text"
-                    placeholder="Apply coupon"
-                    aria-describedby="button-addon3"
-                    class="form-control border-0"
-                  />
-                  <div class="input-group-append border-0">
-                    <button
-                      id="button-addon3"
-                      type="button"
-                      class="btn btn-dark px-4 rounded-pill"
-                    >
-                      <i class="fa fa-gift mr-2"></i>Apply coupon
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">
-                Instructions for seller
-              </div>
-              <div class="p-4">
-                <p class="font-italic mb-4">
-                  If you have some information for the seller you can leave them
-                  in the box below
-                </p>
-                <textarea
-                  name=""
-                  cols="30"
-                  rows="2"
-                  class="form-control"
-                ></textarea>
-              </div>
-            </div> */}
             <div class="col-sm-12">
               <div class="bg-light rounded-pill px-4 py-3 text-uppercase font-weight-bold">
                 Order summary{" "}
@@ -200,19 +97,19 @@ class Cart extends Component {
                 <ul class="list-unstyled mb-4">
                   <li class="d-flex justify-content-between py-3 border-bottom">
                     <strong class="text-muted">Order Subtotal </strong>
-                    <strong>$390.00</strong>
+                                    <strong>{`$${finalPrice}`}</strong>
                   </li>
                   <li class="d-flex justify-content-between py-3 border-bottom">
                     <strong class="text-muted">Shipping and handling</strong>
                     <strong>$10.00</strong>
                   </li>
-                  <li class="d-flex justify-content-between py-3 border-bottom">
+                  {/* <li class="d-flex justify-content-between py-3 border-bottom">
                     <strong class="text-muted">Tax</strong>
                     <strong>$0.00</strong>
-                  </li>
+                  </li> */}
                   <li class="d-flex justify-content-between py-3 border-bottom">
                     <strong class="text-muted">Total</strong>
-                    <h5 class="font-weight-bold">$400.00</h5>
+                                    <h5 class="font-weight-bold">{`$${finalPrice + 10}`}</h5>
                   </li>
                 </ul>
                 <a href="#" class="btn btn-dark rounded-pill py-2 btn-block">
